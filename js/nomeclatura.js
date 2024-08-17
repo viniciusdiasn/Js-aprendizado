6b17818419d615fe0f665a7834d4315060fa9da4
viniciusdiasn <168144967+viniciusdiasn@users.noreply.github.com>
2024-08-17 13:30:28 -0300

    Add files via upload

---
+++ js/nomeclatura.js
@@ -0,0 +1,39 @@
+//tipos de variaveis
+
+var a = "chocolate";
+let b = "laranja";
+const c = 10
+// não pode receber um novo valor
+const d = "Não deu certo"
+
+
+console.log(a)
+console.log(c)
+//tipos de console
+console.log()
+console.warn()
+console.error()
+console.table()
+//  Limpar consoles após a visualização
+// console.clear("limpei")
+
+// Tipos de Dados 
+// string 
+const name = "Meu nome"
+
+// Boolean
+const isApproved = false
+
+// nulo
+const lastname= null
+
+//undefined
+let address
+
+//object
+const brand = {
+    name: "nike",
+    size: ["p","m","g"]
+}
+
+
